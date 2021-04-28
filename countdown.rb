cf4c69975f08dc1055685716b4b90aa538f1e3ee
Lj <tnedutsl457892@gmail.com>
2021-04-28 22:40:01 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,9 @@
 #write your code here
-
+count = 10
 
 def countdown(number)
-  while countdown > 1
-    countdown -= 1
+  while number > 1
+    count -= 1
     puts "#{number} SECOND(S)!"
     puts "HAPPY NEW YEAR!"
   end
